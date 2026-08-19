f92f4134b9970cafd8c9b2e7adf3cc4c66b03021
Pratyush Raj <pratyush7109@gmail.com>
2026-08-19 23:56:08 +0530

    AWS amplify connected, generated models for note

--- app/src/main/java/com/example/mynotesv2/MainActivity.kt
+++ app/src/main/java/com/example/mynotesv2/MainActivity.kt
@@ -26,7 +26,6 @@ class MainActivity : ComponentActivity() {
 
                 val navController = rememberNavController()
                 NavGraph(navController = navController)
-
             }
         }
     }
